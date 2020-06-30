e190f09c3e22d654c3f79cb34587fa0dbf71f833
tanviredu <ornobtanvir.git@gmail.com>
2020-06-30 14:24:07 +0600

    added icon

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div id="main-app">
-      <h4 class="display-4"> {{title}} </h4>
+    <div class="container">
+      <h1 class="display-4">Appoinment List </h1>
+      <font-awesome-icon icon="plus" class="mr-2"></font-awesome-icon>
+      Add Appoinment
+
+    </div>
   </div>
 </template>
 
 <script>
+
+// import all th component you create
+
+// this will import athe icon that you import in the
+// main.js as a component so you can use in the vue as a component
+
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
 export default {
   name: 'MainApp',
   data:function(){
     return {
     title: "Welcome to the Appoinment App"
-  }
+          }
   },
+  components : {
+    // register the components here
+    FontAwesomeIcon
+  }
+
   
 }
 </script>
