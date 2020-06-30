e48e49dc9b17418ff06b61cd38c02629330e35a0
tanviredu <ornobtanvir.git@gmail.com>
2020-06-30 15:03:31 +0600

    added

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,13 @@
       <h1 class="display-4">Appoinment List </h1>
       <font-awesome-icon icon="plus" class="mr-2"></font-awesome-icon>
       Add Appoinment
+      <hr>
+      <div v-for="(item,index) in appoinments" v-bind:key="index">      
+          <h4> {{item.petName}}</h4>
+          <p> {{item.aptNotes}}</p>
 
+        
+      </div>
     </div>
   </div>
 </template>
@@ -18,16 +24,30 @@
 
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
+// import the data from api or from some folder
+// with the get request
+import axios from "axios"
+
 export default {
   name: 'MainApp',
   data:function(){
     return {
-    title: "Welcome to the Appoinment App"
+    title: "Welcome to the Appoinment App",
+    appoinments: [],
           }
   },
   components : {
     // register the components here
     FontAwesomeIcon
+  },
+  // anything you put inside mounted will be loaded
+  // at the starting of the program
+  mounted(){
+      axios.get("https://raw.githubusercontent.com/tanviredu/vue-interface/master/public/data/appointments.json")
+      .then((res)=>{
+          this.appoinments = res.data
+
+      })
   }
 
   
